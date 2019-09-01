82107c24d3b649154a284d2f5dbb9f6710a3e39d
Nazareno Andrade <nazarenoandrade@MacBook-Pro-de-Nazareno.local>
2019-09-01 12:16:33 -0500

    Revert "É dada a largada. E 18 lives de bolsonaro"
    This reverts commit b6d77aa18aeaefd6e08d4c0e8eb849f1bc3baa69.

--- code/clean_ccs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tail -n +4 "$*" | grep -v '^00' | sed -e 's/<[^>]*>//g' | sed '/^ *$/d' | uniq | tr '\n' ' ' 
--- code/download_ccs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-youtube-dl --sub-lang pt --write-auto-sub --skip-download $1
